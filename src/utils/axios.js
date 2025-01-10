4676827822463648065fd22cc977ffc94105c859
roshith <roshithprakash7@gmail.com>
2025-01-10 19:59:05 +0530

    Adding new Prod URL

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,8 +2,9 @@ import axios from "axios"
 
 export const devURL = "http://localhost:4000"
 export const prodURL = "https://blog-backend-3u5c.onrender.com"
+export const newProd = "https://blog-backend-seven-delta.vercel.app/"
 
 // Creating an instance of axios to make API calls to server
 export const axiosInstance = axios.create({
-    baseURL: prodURL
+    baseURL: newProd
 })

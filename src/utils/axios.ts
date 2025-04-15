9a378f19a6b87c64740219f84ef8e9189d348d8b
roshith-prakash <roshithprakash7@gmail.com>
2025-04-15 23:10:16 +0530

    URL change

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -5,5 +5,5 @@ export const prodURL = "https://blog-backend-seven-delta.vercel.app/";
 
 // Creating an instance of axios to make API calls to server
 export const axiosInstance = axios.create({
-  baseURL: devURL,
+  baseURL: prodURL,
 });
